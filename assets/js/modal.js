562d3bbdd5ffa62cc48a3b3e5bc9db4aa87d3ab1
Sergey13233 <100700452+Sergey13233@users.noreply.github.com>
2026-01-16 14:06:22 +0300

    Add files via upload

---
+++ assets/js/modal.js
@@ -0,0 +1,63 @@
+const modalBtn = document.querySelectorAll('[data-modal]');
+const body = document.body;
+const modalClose = document.querySelectorAll('.modal__close');
+const modal = document.querySelectorAll('.modal');
+
+
+modalBtn.forEach(item => {
+    item.addEventListener('click', event => {
+        let $this = event.currentTarget;
+        let modalId = $this.getAttribute('data-modal');
+        let modal = document.getElementById(modalId);
+        let modalContent = modal.querySelector('.modal__content');
+
+        modalContent.addEventListener('click', event => {
+            event.stopPropagation();
+        });
+
+        modal.classList.add('show');
+        body.classList.add('no-scroll');
+
+        setTimeout(function() {
+            modalContent.style.transform = 'none';
+            modalContent.style.opacity = '1';
+        }, 1);
+
+        document.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape' || e.key === 'Esc') {
+                console.log('Escape нажата');
+                modal.classList.remove('show');
+                body.classList.remove('no-scroll');
+            }
+        });
+    });
+});
+
+
+modalClose.forEach(item => {
+    item.addEventListener('click', event => {
+        let currentModal = event.currentTarget.closest('.modal');
+
+        closeModal(currentModal);
+    });
+});
+
+
+modal.forEach(item => {
+    item.addEventListener('click', event => {
+        let currentModal = event.currentTarget;
+
+        closeModal(currentModal);
+    });
+});
+
+
+function closeModal(currentModal) {
+    let modalContent = currentModal.querySelector('.modal__content');
+    modalContent.removeAttribute('style');
+
+    setTimeout(() => {
+        currentModal.classList.remove('show');
+        body.classList.remove('no-scroll');
+    }, 200);
+}
